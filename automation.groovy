46e00a13811bd01eff2aac8d83ad167d0de50afa
iejitagha <33166119+iejitagha@users.noreply.github.com>
2020-04-05 11:01:10 +0100

    Update automation.groovy

--- automation.groovy
+++ automation.groovy
@@ -33,117 +33,16 @@ pipeline {
         stage('prepare') {
             steps {
                 cleanWs()
-                git url: env.awsm_code, branch: params.Code_branch
-                dir('common') {
-                    sh 'docker build --file Dockerfiles/ajv --tag ajv .'
-                    sh 'docker build --file Dockerfiles/infr --tag taurus/cli .'
-                }
+                sh "echo \"This is a test\""
             }
         }
 
-        stage('planning') {
-            steps {
-                withCredentials([file(credentialsId: 'svc_account_management_worker', variable: 'CREDS_FILE')]) {
-                    sh "docker run --rm -v $CREDS_FILE:/root/.aws/credentials -v `pwd`:/app -w /app/terraform/cloudfoundation-automation taurus/cli ./cloudautomation-plan-npe.sh"
-            }
-        }
-
-
-        stage('human_initiate') {
-            steps {
-                script {
-                    try {
-                        timeout(time: 12, unit: 'HOURS') {
-                            input 'Initiate "terraform apply" of the plan above?'
-                        }
-                    }
-                    catch (e) {
-                        currentBuild.result = 'UNSTABLE'
-                    }
-                }
-            }
-        }
-
-        stage('applying') {
-            steps {
-                withCredentials([file(credentialsId: 'svc_account_management_worker', variable: 'CREDS_FILE')]) {
-                    sh "docker run --rm -v $CREDS_FILE:/root/.aws/credentials -v `pwd`:/app -w /app/terraform/cloudfoundation-automation taurus/cli ./cloudautomation-apply-npe.sh"
-                    sh "sleep 30"
-                }
-            }
-        }
-
-        // Run API Test pipeline here
-
-        stage('planning production') {
-            steps {
-                withCredentials([file(credentialsId: 'svc_account_management_worker', variable: 'CREDS_FILE')]) {
-                    sh "docker run --rm -v $CREDS_FILE:/root/.aws/credentials -v `pwd`:/app -w /app/terraform/cloudfoundation-automation taurus/cli ./cloudautomation-plan-prod.sh"
-                }
-                script {
-                    if (params.Code_branch != "master") {
-                        echo 'Skipping the rest since branch is not "master".'
-                        currentBuild.result = 'UNSTABLE'
-                    }
-                }
-            }
-        }
-
-
-        stage('human_approve_production') {
-            input {
-                id 'peer_reviewed'
-                message 'Are the correct parameters supplied?'
-                ok 'Yes, I have checked the parameters supplied.'
-                submitterParameter 'APPROVER'
-            }
-            steps {
-                echo ''
-            }
-            post {
-                always {
-                    wrap([$class: 'BuildUser']) {
-                        script {
-                            if (env.BUILD_USER_ID == env.APPROVER) {
-                                echo 'ERROR - Operator cannot self approve this change!'
-                                currentBuild.result = 'UNSTABLE'
-                            }
-                        }
-                    }
-                }
-            }
-
-        }
-
-        stage('human_initiate for production') {
-            steps {
-                script {
-                    try {
-                        timeout(time: 12, unit: 'HOURS') {
-                            input 'Initiate "terraform apply" of the plan above?'
-                        }
-                    }
-                    catch (e) {
-                        currentBuild.result = 'UNSTABLE'
-                    }
-                }
-            }
-        }
-
-        stage('applying production') {
-            steps {
-                withCredentials([file(credentialsId: 'svc_account_management_worker', variable: 'CREDS_FILE')]) {
-                    sh "docker run --rm -v $CREDS_FILE:/root/.aws/credentials -v `pwd`:/app -w /app/terraform/cloudfoundation-automation taurus/cli ./cloudautomation-apply.sh"
-                    sh "sleep 30"
-                }
-            }
-        }
-
+        
         // Run API Test pipeline here
 
         stage('cleanup') {
             steps {
-                sh 'docker run --rm -v `pwd`:/app -w /app/aws-iam/roles/ taurus/cli chmod a+rwX --recursive /app'
+                sh "echo \"This is a the clean up step\""
             }
         }
     }
